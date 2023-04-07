d66ca02bea9db6bc7acfc9b20aa62b960bedaff4
Zaris47 <akhildasmisterio@gmail.com>
2023-04-07 18:17:20 +0530

    Moved text as global

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,7 +1,7 @@
 import {useState} from 'react'
 import { Textarea, Button, useToast } from '@chakra-ui/react'
 
-const TextInput = () => {
+const TextInput = ( {extractKeywords}) => {
 
     const [text, setText]= useState('');
 
@@ -19,7 +19,7 @@ const TextInput = () => {
                  });
         }
         else{
-            console.log(text);
+            extractKeywords(text);
         }
     }
   return (
